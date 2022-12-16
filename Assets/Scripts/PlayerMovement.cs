ce53a96838e2c4d18b0fe7d4df2d6b2456814eb4
azphalt59 <62052602+azphalt59@users.noreply.github.com>
2022-12-16 17:52:06 +0100

    WIP

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public GameObject PlayerMainGameObject;
-    public float MovementSpeed;
+    [Header("References")]
+    [SerializeField] private GameObject playerMainGameObject;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private SpriteRenderer playerSprite;
+
+    [Header("Movement")]
+    public float WalkSpeed;
+    public float RunSpeed;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +23,47 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
         Vector3 movementDirection = new Vector3(horizontalMovement, verticalMovement);
-        transform.position += movementDirection.normalized * MovementSpeed * Time.deltaTime;
+        Vector3 movementToAdd;
+
+        // Set Movement
+        if(Input.GetKey(KeyCode.Space))
+        {
+            movementToAdd = movementDirection.normalized * RunSpeed * Time.deltaTime;
+            playerAnimator.speed = 2f;
+            
+        }
+        else
+        {
+            movementToAdd = movementDirection.normalized * WalkSpeed * Time.deltaTime;
+            playerAnimator.speed = 1f;
+        }
+
+        // Set Idle
+        if(movementToAdd.magnitude <= 0)
+        {
+            playerAnimator.SetBool("Idle", true);
+            playerAnimator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            playerAnimator.SetBool("Idle", false);
+            playerAnimator.SetBool("IsWalking", true);
+        }
+            
+        // Flip sprite
+        if (horizontalMovement < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        if(horizontalMovement > 0)
+        {
+            playerSprite.flipX = false;
+        }
+       
+        transform.position += movementToAdd;
     }
+    
 }
